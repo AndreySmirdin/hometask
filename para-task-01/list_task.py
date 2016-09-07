dffa430df91cc6ee2a6ad19d133cac2e16b36a54
AndreySmirdin <andrey.smirdin@mail.ru>
2016-09-08 00:09:31 +0300

    trying to commit

---
+++ para-task-01/list_task.py
@@ -0,0 +1,20 @@
+# Remove equal adjacent elements
+#
+# Example input: [1, 2, 2, 3]
+# Example output: [1, 2, 3]
+def remove_adjacent(lst):
+    return
+
+# Merge two sorted lists in one sorted list in linear time
+#
+# Example input: [2, 4, 6], [1, 3, 5]
+# Example output: [1, 2, 3, 4, 5, 6]
+def linear_merge(lst1, lst2):
+    return
+
+list = [1, 3, 2]
+list.sort();
+for i in list[]
+    print(i)
+print(list);
+
