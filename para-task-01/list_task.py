774c58fcad3722cf3832431189e13252275e60ff
AndreySmirdin <andrey.smirdin@mail.ru>
2016-09-09 22:44:47 +0300

    first hometask

--- para-task-01/list_task.py
+++ para-task-01/list_task.py
@@ -3,18 +3,30 @@
 # Example input: [1, 2, 2, 3]
 # Example output: [1, 2, 3]
 def remove_adjacent(lst):
-    return
+    ans = []
+    ans.append(lst[0])
+    last = lst[0]
+    for i in lst:
+        if last != i:
+            ans.append(i)
+        last = i;
+    return ans
 
 # Merge two sorted lists in one sorted list in linear time
 #
 # Example input: [2, 4, 6], [1, 3, 5]
 # Example output: [1, 2, 3, 4, 5, 6]
 def linear_merge(lst1, lst2):
-    return
+    ans = []
+    a = 0
+    b = 0
+    while a < len(lst1) or b < len(lst2):
+        if a != len(lst1) and (b == len(lst2) or lst1[a] < lst2[b]):
+            ans.append(lst1[a])
+            a += 1
+        else:
+            ans.append(lst2[b])
+            b += 1       
+    return ans
 
-list = [1, 3, 2]
-list.sort();
-for i in list[]
-    print(i)
-print(list);
 
