7ea615ae046be9e8ccd83c2eacf45e65ac874af9
AndreySmirdin <andrey.smirdin@mail.ru>
2016-09-18 21:03:06 +0300

    new separators and reading

--- para-task-02/copies.py
+++ para-task-02/copies.py
@@ -3,26 +3,29 @@ import sys, hashlib, os
 def read_and_hash(name):
 	digest = hashlib.md5()
 	with open(name, "rb") as f:
-		digest.update(f.read(1000))
+		new = f.read(1000)
+		while new:
+			digest.update(new)
+			new = f.read(1000)
 	return digest.hexdigest()	
 
 def find_copies(dir):
-	tree = os.walk(dir)
 	dic = {}
-	for root, dirs, files in tree:
+	for root, dirs, files in os.walk(dir):
 		for file in files:
-			if file[0] != "." and file[0] != "~":
-				name = dir + '/'.join(dirs) + "/" + file
+			if file[0] != "." and file[0] != "~" and  not os.path.islink(file):
+				name = os.path.join(root, file)
 				hashed = read_and_hash(name)
 				if not(hashed in dic):
 					dic[hashed] = []
 				dic[hashed].append(name)
-	print_copies(dic)
+	return dic
 
-def print_copies(dic):
-	for k, v in dic.items():
+def print_copies(hash_list):
+	for v in hash_list.values():
 		if len(v) > 1:
 			print(':'.join(v))
 
 dir = sys.argv[1]
-find_copies(dir)
+hash_list = find_copies(dir)
+print_copies(hash_list)
