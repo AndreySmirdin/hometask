e5f5ec09d228cbcb6d7fbd00deaa23d69e040bf8
AndreySmirdin <andrey.smirdin@mail.ru>
2016-09-27 19:50:25 +0300

    split functiom

--- para-task-03/strassen.py
+++ para-task-03/strassen.py
@@ -2,25 +2,25 @@ import numpy as np
 import sys
 
 
-def read_matrix(m, n):
-    m[:n, :n] = np.loadtxt(sys.stdin)
-    return m
+def read_matrix(size, real_size):
+    matrix = np.zeros((size, size), dtype=int)
+    matrix[:real_size, :real_size] = np.loadtxt(sys.stdin)
+    return matrix
 
 
-def add_zeros(size):
-    m = np.zeros((size, size), dtype=int)
-    return m
+def split(matrix):
+    matrix_up, matrix_down = np.hsplit(matrix, 2)
+    matrix11, matrix21 = np.vsplit(matrix_up, 2)
+    matrix12, matrix22 = np.vsplit(matrix_down, 2)
+    return matrix11, matrix12, matrix21, matrix22
 
 
 def mult(m1, m2):
     if m1.shape[0] == 1:
         return m1 * m2
-    a_up, a_down = np.hsplit(m1, 2)
-    a11, a21 = np.vsplit(a_up, 2)
-    a12, a22 = np.vsplit(a_down, 2)
-    b_up, b_down = np.hsplit(m2, 2)
-    b11, b21 = np.vsplit(b_up, 2)
-    b12, b22 = np.vsplit(b_down, 2)
+
+    a11, a12, a21, a22 = split(m1)
+    b11, b12, b21, b22 = split(m2)
     p1 = mult(a11 + a22, b11 + b22)
     p2 = mult(a21 + a22, b11)
     p3 = mult(a11, b12 - b22)
@@ -34,18 +34,19 @@ def mult(m1, m2):
     c21 = p3 + p5
     c22 = p1 - p2 + p3 + p6
 
-    return np.hstack((np.vstack((c11, c12)), np.vstack((c21, c22))))
+    return np.hstack((np.vstack((c11, c12)),
+                      np.vstack((c21, c22))))
 
 
 def print_ans(ans):
     for i in ans:
         print(' '.join(map(str, i)))
 
-n = int(input())
+real_size = int(input())
 size = 1
-while size < n:
+while size < real_size:
     size *= 2
-m1 = read_matrix(add_zeros(size), n)
-m2 = read_matrix(add_zeros(size), n)
+m1 = read_matrix(size, real_size)
+m2 = read_matrix(size, real_size)
 ans = mult(m1, m2)
-print_ans(ans[:n, :n])
+print_ans(ans[:real_size, :real_size])
