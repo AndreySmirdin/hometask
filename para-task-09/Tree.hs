ed414e034f875e360239f0fd539349ecf2138776
AndreySmirdin <andrey.smirdin@mail.ru>
2016-11-29 00:18:33 +0300

    insert fixed

--- para-task-09/Tree.hs
+++ para-task-09/Tree.hs
@@ -3,7 +3,7 @@ import Prelude hiding (lookup)
 data BinaryTree k v = Nil | Cons (k, v) (BinaryTree k v) (BinaryTree k v) deriving (Show, Eq)
 
 insert k v Nil = Cons (k, v) Nil Nil
-insert k v (Cons node@(k', v') tree1 tree2) | k == k' = Cons node tree1 tree2
+insert k v (Cons node@(k', v') tree1 tree2) | k == k' = Cons (k, v) tree1 tree2
                                             | k < k'  = Cons node (insert k v tree1) tree2
                                             | k > k'  = Cons node tree1 (insert k v tree2)
 
@@ -19,5 +19,4 @@ delete k (Cons node@(k', v) tree1 tree2) | k == k' = merge tree1 tree2
                                          | k > k'  = Cons node tree1 (delete k tree2)
       where merge Nil tree = tree
             merge (Cons node tree11 tree12) tree2 = Cons node tree11 (merge tree12 tree2)
-
 
